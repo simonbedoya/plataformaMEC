6a1fef08109328addc5f48df5851c3b9aa09c36e
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2016-12-21 16:02:38 -0500

    plataforma mec 3

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods',"GET,PUT,POST,DELETE,OPTIONS");
+    res.header('Access-Control-Allow-Methods',"GET,PUT,POST,DELETE");
     res.header('Access-Control-Allow-Headers', "*");
     next();
 });
