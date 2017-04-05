4f5800dfe55995e8e852eaab0312e8ef78ced887
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-05 17:15:46 -0500

    05-04-2017 2

--- controller/api/upload_controller.js
+++ controller/api/upload_controller.js
@@ -18,7 +18,7 @@ module.exports = {
                 let date = functions.convertDate(file_name_array[0]);
                 let hour = functions.convertHour(file_name_array[1]);
                 let reg_date = functions.datetime();
-                db.query(template(sqlQuery.query_insertFile,{pk_sensor: pk_sensor, path_file: path_file, date: date, hour: hour, reg_date: reg_date, pk_location: pk_location}), function (err, result) {
+                db.query(template(sqlQuery.query_insertFile,{pk_sensor: pk_sensor, path_file: path_file, date: date, hour: hour, reg_date: reg_date, pk_location: pk_location, axis: file_name_array[2]}), function (err, result) {
                     if (err) return fullfill({hcode: 500, code: "005", msg: "Internal error insert", data: null});
 
                     if(result.affectedRows !== 0){
@@ -37,7 +37,7 @@ module.exports = {
               let date = functions.convertDate(file_name_array[0]);
               let hour = functions.convertHour(file_name_array[1]);
               let reg_date = functions.datetime();
-              db.query(template(sqlQuery.query_updateFile,{pk_sensor: pk_sensor, path_file: path_file, date: date, hour: hour, reg_date: reg_date}), function (err, result) {
+              db.query(template(sqlQuery.query_updateFile,{pk_sensor: pk_sensor, path_file: path_file, date: date, hour: hour, reg_date: reg_date, axis: file_name_array[2]}), function (err, result) {
                   if (err) return fullfill({hcode: 500, code: "005", msg: "Internal error update", data: null});
 
                   if(result.affectedRows !== 0){
@@ -56,7 +56,7 @@ module.exports = {
                 let file_name_array = file_name.split("_");
                 let date = functions.convertDate(file_name_array[0]);
                 let hour = functions.convertHour(file_name_array[1]);
-                db.query(template(sqlQuery.query_verifyExistFile,{pk_sensor: pk_sensor, date: date, hour: hour}), function (err, result) {
+                db.query(template(sqlQuery.query_verifyExistFile,{pk_sensor: pk_sensor, date: date, hour: hour, axis: file_name_array[2]}), function (err, result) {
                     if (err) return fullfill({hcode: 500, code: "005", msg: "Internal error exist file", data: null});
 
                     if(result[0].counter === 0){
