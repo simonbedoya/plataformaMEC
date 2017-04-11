a35d3986d1a58badde0ce544c6ec4a2a96ee3f2a
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-11 18:11:48 -0500

    11-04-2017 7

--- routes/log-in.js
+++ routes/log-in.js
@@ -4,32 +4,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db/connection');
-//var dbDao = require('../db/dbDao');
-//var mysql = require('mysql');
-
-/*var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'dbserveriot.cgud7zqnytaf.us-west-2.rds.amazonaws.com',
-    user            : 'dbserveriot',
-    password        : 'iot12345',
-    database        : 'dbserveriot'
-});
-
-pool.getConnection(function (err, connection) {
-   if(err){
-       console.error(err);
-   }else{
-       //console.log(connection);
-       connection.query('SELECT * FROM TBL_USER', function(err, rows, fields) {
-           if (err) throw err;
-
-           console.log('The solution is: ', rows[0]);
-       });
-   }
-
-});*/
-
-
+const sqlQuery = require('../sql/sql_data');
+const template = require('es6-template-strings');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
@@ -40,7 +16,7 @@ router.post('/', function(req, res, next) {
     var email = req.body.email;
     var pass = req.body.password;
     var sql = "SELECT count(*) AS counter FROM TBL_USER WHERE EMAIL_USER = '"+email+"' AND PASSWORD_USER = '"+pass+"'";
-    db.query(sql,function (err,result) {
+    db.query(template(sqlQuery.query_login,{email: email, pass:pass}),function (err,result) {
         console.log(result[0].counter);
         if (err){
             var string = encodeURIComponent('Problemas con el servidor');
