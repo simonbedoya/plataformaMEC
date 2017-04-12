924ed283556cd96b0cc7a91c6580703ad855b464
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-12 15:52:20 -0500

    12-04-2017 6

--- public/js/functions/graphic.js
+++ public/js/functions/graphic.js
@@ -18,7 +18,7 @@ socket.on("connection_success", function (data) {
     console.log(data);
 });
 
-socket.emit('register','{simon: "hola"}');
+socket.emit('register','{"simon": "hola"}');
 
 
 function displayGrphicRT() {
