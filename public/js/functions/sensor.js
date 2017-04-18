dd60f3f0a090593505896b9c55d2a8fca8b3f07b
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-17 23:35:05 -0500

    commit

--- public/js/functions/sensor.js
+++ public/js/functions/sensor.js
@@ -888,7 +888,7 @@ $("#real_time_graphic").on('shown.bs.modal', function () {
             "gridAlpha": 0.15,
             "minorGridEnabled": true,
             "axisColor": "#DADADA",
-            "dateFormats": [{"period":"fff","format":"JJ:NN:SS"},{"period":"ss","format":"JJ:NN:SS"}]
+            "dateFormats": [{"period":"fff","format":"JJ:NN:SS"},{"period":"ss","format":"JJ:NN:SS"},{period:'mm',format:'JJ:NN'}]
         },
         "valueAxes": [{
             "axisAlpha": 0.2,
