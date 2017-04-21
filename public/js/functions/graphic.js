813002b9b4fafb82ae1fdae307c1241b821226ac
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-21 09:56:17 -0500

    21/04/17

--- public/js/functions/graphic.js
+++ public/js/functions/graphic.js
@@ -755,9 +755,11 @@ function getdata(dataNew) {
         let date = new Date();
         date.setHours(0, 0, 1, 0);
         let minutes = parseInt(i.x / 60);
-        let seconds = i.x % 60;
+        let seconds = parseInt(i.x % 60);
+        let mili = parseInt((i.x % 60) - parseInt(i.x %60));
         date.setMinutes(minutes);
         date.setSeconds(seconds);
+        date.setMilliseconds(mili);
 
         let dataFinale = {
             "date": date,
