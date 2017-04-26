3282ee3c14dfbc3533aa2e4e0916f77ef167785e
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-26 01:16:38 -0500

    23/04/17

--- controller/data_controller.js
+++ controller/data_controller.js
@@ -386,7 +386,7 @@ module.exports = {
                     if (err) return fullfill({hcode: 202, code: "003", msg: "Error", data: null});
 
                     if (result.length !== 0) {
-                        fullfill({hcode: 200, code: "001", msg: "details", data: result[0]});
+                        fullfill({hcode: 200, code: "001", msg: "details", data: result});
                     } else {
                         fullfill({hcode: 202, code: "002", msg: "no hay datos", data: null});
                     }
