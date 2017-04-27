1ff6120e7b10569e5fcd40e38a6c3e163f9df1fe
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-27 14:47:30 -0500

    27/04/17 3

--- controller/data_controller.js
+++ controller/data_controller.js
@@ -419,7 +419,7 @@ module.exports = {
                     if (err) return fullfill({hcode: 202, code: "003", msg: "Error", data: null});
 
                     if (result.length !== 0) {
-                        fullfill({hcode: 200, code: "001", msg: "notifications", data: JSON.stringify(result[0])});
+                        fullfill({hcode: 200, code: "001", msg: "notifications", data: JSON.stringify(result)});
                     } else {
                         fullfill({hcode: 202, code: "002", msg: "no hay datos", data: null});
                     }
