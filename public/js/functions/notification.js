00ea58f7e96500865f8f23aedb01b72cce3f8336
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-04-27 22:45:47 -0500

    27/04/17 3

--- public/js/functions/notification.js
+++ public/js/functions/notification.js
@@ -13,6 +13,7 @@ function load(email) {
     showLoadNotification(true);
     loadNotification(email);
     loadNumberNotification(email);
+    loadNumberNoReadNotification(email);
 }
 
 function loadNotification(email) {
@@ -36,6 +37,45 @@ function loadNotification(email) {
                 });
             } else if(result.code === "002"){
                 setDataNotification(null);
+            }
+        },
+        error: function (e) {
+            console.log(e);
+            swal({
+                title: "Información",
+                text: "Ha ocurrido un error intenta de nuevo!",
+                type: "info",
+                showCancelButton: false,
+                confirmButtonColor: "#444a53",
+                confirmButtonText: "OK"
+            }).then(function () {
+
+            });
+        }
+    });
+}
+
+function loadNumberNoReadNotification(email){
+    $.ajax({
+        type: "post",
+        url: "https://plataformamec.com/data/getNotificationNoRead",
+        data: {email: email},
+        success: function (result) {
+            if (result.code === "001") {
+                document.getElementById("numberNoRead").innerHTML = `(${result.data.N_NOTI})`;
+            } else if (result.code === "003") {
+                swal({
+                    title: "Información",
+                    text: "Ha ocurrido un error intenta de nuevo!",
+                    type: "info",
+                    showCancelButton: false,
+                    confirmButtonColor: "#444a53",
+                    confirmButtonText: "OK"
+                }).then(function () {
+
+                });
+            } else if(result.code === "002"){
+                document.getElementById("numberNoRead").innerHTML = `(0)`;
             }
         },
         error: function (e) {
