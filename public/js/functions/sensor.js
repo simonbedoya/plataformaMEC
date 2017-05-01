f40fd18c4d0502d694dcf9b48a9fd699b4cf88a2
SimonBedoyaValencia <simonbedoyavalencia@gmail.com>
2017-05-01 16:37:29 -0500

    01/05/17

--- public/js/functions/sensor.js
+++ public/js/functions/sensor.js
@@ -1344,55 +1344,55 @@ function verifyParamsEvents() {
     let d_pos = parseInt(document.getElementById("d_pos").value);
     let d_min = parseFloat(document.getElementById("d_min").value);
 
-    if(d_w_sta > 0){
+    if(!d_w_sta > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(d_w_lta > 0){
+    if(!d_w_lta > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(t_on > 0){
+    if(!t_on > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(t_off > 0){
+    if(!t_off > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(d_pre > 0){
+    if(!d_pre > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(d_pos > 0){
+    if(!d_pos > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(d_min > 0){
+    if(!d_min > 0){
         showErrorConfigEvent("Todos los valores introducidos deben ser positivos y mayores que cero.",true);
         return false;
     }
-    if(d_w_sta < d_w_lta){
+    if(!d_w_sta < d_w_lta){
         showErrorConfigEvent("La ventana STA debe ser menor a la ventana LTA",true);
         return false;
     }
-    if(d_w_sta <= 60){
+    if(!d_w_sta <= 60){
         showErrorConfigEvent("La ventana STA debe ser menor o igual a un minuto (60 seg).",true);
         return false;
     }
-    if(d_w_lta <= 120){
+    if(!d_w_lta <= 120){
         showErrorConfigEvent("La ventana LTA debe ser menor o igual a 2 minutos (120 seg).",true);
         return false;
     }
-    if(t_off < t_on){
+    if(!t_off < t_on){
         showErrorConfigEvent("Trigger off debe ser menor que trigger on.",true);
         return false;
     }
-    if(d_pre <= 60 && d_pos <= 60){
+    if(!d_pre <= 60 && d_pos <= 60){
         showErrorConfigEvent("La duracion pre y post evento deben ser menores o iguales a 1 minuto (60 seg).",true);
         return false;
     }
-    if(d_min <= 60){
+    if(!d_min <= 60){
         showErrorConfigEvent("La duración minima debe ser menor a 1 minuto (60 seg).",true);
         return false;
     }
