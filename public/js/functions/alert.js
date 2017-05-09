4218bbb06ad80e845fdc8e75bf9daa4105c7ffee
simon bedoya <simonbedoya@unicauca.edu.co>
2017-05-09 05:42:50 -0500

    09/05/17

--- public/js/functions/alert.js
+++ public/js/functions/alert.js
@@ -2,14 +2,22 @@
  * Created by sbv23 on 01/05/2017.
  */
 
-function load(email,networks) {
+function load(email,networks,sensors) {
     reloadNumberNoReadNotification(email);
     loadNetwork(networks);
+    loadSensor(sensors);
 }
 
 function loadNetwork(data) {
     let i;
     for(i = 0; i < data.length; i++) {
         document.getElementById("filterNetwork").innerHTML += "<option value='" + data[i].PK_NETWORK + "'>"+data[i].NAME_NETWORK+"</option>";
+    }
+}
+
+function loadSensor(data) {
+    let i;
+    for(i = 0; i < data.length; i++) {
+        document.getElementById("filterSensor").innerHTML += "<option value='" + data[i].PK_SENSOR + "'>"+data[i].NAME_SENSOR+"</option>";
     }
 }
