31fa632b273439bba3931bed59ed0309bdafbede
simon bedoya <simonbedoya@unicauca.edu.co>
2017-05-09 05:44:04 -0500

    09/05/17

--- sql/sql_alert.js
+++ sql/sql_alert.js
@@ -3,5 +3,5 @@
  */
 module.exports = {
     getNetowrkByUser: "SELECT PK_NETWORK, NAME_NETWORK FROM TBL_NETWORK WHERE EMAIL_USER = '${email}'",
-    getSensorByUser: "SELECT tbs.PK_SENSOR, tbs.NAME_SENSOR FROM TBL_SENSOR tbs INNER JOIN TBL_NETWORK tbn ON tbs.PK_NETWORK = tbn.PK_NETWORK WHERE tbn.EMAIL_USER = '${email}'"
+    getSensorByUser: "SELECT tbs.PK_SENSOR, tbs.NAME_SENSOR FROM TBL_SENSOR tbs INNER JOIN TBL_NETWORK tbn ON tbs.PK_NETWORK = tbn.PK_NETWORK WHERE tbn.EMAIL_USER = '${email}' AND tbs.STATUS_SENSOR = 'Activo'"
 };
