a951817e191f5aa14a4a4af9a9ac40f08ce52fa5
simon bedoya <simonbedoya@unicauca.edu.co>
2017-05-09 14:21:06 -0500

    09/05/17

--- public/js/functions/alert.js
+++ public/js/functions/alert.js
@@ -66,3 +66,211 @@ function loadEvents(dataList) {
         "columns" : [{"data": "DATE"},{"data": "HOUR"},{"data": "AXIS"},{"data": "NAME"},{"data": "OPTION"}]
     });
 }
+
+function readFile(pk_file,hour,axis) {
+    let data = [];
+    $.ajax({
+        type: "post",
+        url: "https://plataformamec.com/data/getDataFileByPk",
+        data: {pk_file: pk_file},
+        success: function (result) {
+            if(result.code === "001") {
+                let date_file = result.msg.split("T");
+                let resultArray = result.data.split("\n");
+
+                let delta = resultArray[0].split(" : ");
+                let time = parseFloat(delta[1]);
+                let samplesec = parseInt(1/time);
+                let samples = resultArray[3].split(" : ");
+                let duration = parseInt((time * parseInt(samples[1])) / 60);
+                ymax = resultArray[1].split(" : ");
+                ymin = resultArray[2].split(" : ");
+                for (let i = 4; i < resultArray.length - 1; i++) {
+                    let arrAux = resultArray[i].split(" = ");
+                    let arrCom = {x: time * (i - 4), y: parseFloat(arrAux[1])};
+                    data.push(arrCom);
+                }
+                loadSamplesDialog(samplesec);
+            }
+
+        },
+        error: function (e) {
+            console.log(e);
+            swal({
+                title: "Información",
+                text: "Ha ocurrido un error intenta de nuevo!",
+                type: "info",
+                showCancelButton: false,
+                confirmButtonColor: "#444a53",
+                confirmButtonText: "OK"
+            }).then(function () {
+                readFile(pk_file);
+            });
+        }
+    });
+}
+
+function loadSamplesDialog(samples) {
+    let arraySamples;
+    if(samples === 40){
+        arraySamples =  {'1': '1','2': '2','4': '4','5': '5','8': '8','10': '10','20': '20','40': '40'};
+    }else if(samples === 50){
+        arraySamples =  {'1': '1','2': '2','5': '5','10': '10','50': '50'};
+    }else if(samples === 100){
+        arraySamples =  {'1': '1','2': '2','4': '4','5': '5','10': '10','20': '20','25': '25','50': '50','100': '100'};
+    }else if(samples === 200){
+        arraySamples =  {'1': '1','2': '2','4': '4','5': '5','10': '10','20': '20','25': '25','50': '50','100': '100','200': '200'};
+    }
+    swal({
+        title: 'Seleccione una tasa de muestreo para generar la grafica:',
+        input: 'select',
+        inputOptions: arraySamples,
+        inputPlaceholder: 'Muestras por segundo',
+        showCancelButton: true
+    }).then(function (result) {
+        alert(result);
+    })
+}
+
+function generateGraphic() {
+
+    let dataNew = [];
+    let samples = parseInt($('#samInp').val());
+    let interJump = parseInt(maxSamples / samples);
+
+    if((maxSamples % samples) !== 0){
+        swal({
+            title: "Información",
+            text: `El numero de muestras no es exacto, se realizara la grafica con un muestreo igual a ${parseInt(maxSamples / interJump)} sps!`,
+            type: "info",
+            showCancelButton: false,
+            confirmButtonColor: "#444a53",
+            confirmButtonText: "OK"
+        }).then(function () {
+
+        });
+    }
+
+    for (let i = 0; i < data.length; i = i + interJump) {
+        dataNew.push(data[i]);
+    }
+    drawGraphic(dataNew);
+}
+
+let chart;
+
+function drawGraphic(dataNew) {
+
+    if(dataNew.length === 0){
+        swal({
+            title: "Información",
+            text: `Ha seleccionado un rango de tiempo fuera del los datos del archivo!`,
+            type: "info",
+            showCancelButton: false,
+            confirmButtonColor: "#444a53",
+            confirmButtonText: "OK"
+        }).then(function () {
+
+        });
+    }else{
+        let valueAxes = [{'axisAlpha': 0.2,'id': 'g1'}];
+        let graphs = [{
+            "id": "g1",
+            "valueField": axis,
+            "bullet": "round",
+            "balloonText": `${axis} : [[value]]`,
+            "title": axis,
+            "bulletBorderColor": "#FFFFFF",
+            "bulletBorderThickness": 2,
+            "lineThickness": 2,
+            "lineColor": "#08088A",
+            "negativeLineColor": "#08088A",
+            "hideBulletsCount": 50
+        }];
+
+        chart = AmCharts.makeChart( "chartdiv", {
+            "type": "serial",
+            "theme": "light",
+            "language": "es",
+            "zoomOutButton": {
+                "backgroundColor": '#000000',
+                "backgroundAlpha": 0.15
+            },
+            "dataProvider": getdata(dataNew),
+            "categoryField": "date",
+            "categoryAxis": {
+                "parseDates": true,
+                "minPeriod": "fff",
+                "dashLength": 1,
+                "gridAlpha": 0.15,
+                "minorGridEnabled": true,
+                "axisColor": "#DADADA",
+                "dateFormats": [{"period":"fff","format":"JJ:NN:SS"},{"period":"ss","format":"JJ:NN:SS"},{period:'mm',format:'JJ:NN'},{period:'hh',format:'JJ:NN'}]
+            },
+            "valueAxes": valueAxes,
+            "graphs": graphs,
+            "chartCursor": {
+                "cursorAlpha": 0,
+                "zoomable": false,
+                "valueZoomable":true
+            },
+            "chartScrollbar": {
+                "graph": "g1",
+                "scrollbarHeight": 40,
+                "color": "#FFFFFF",
+                "autoGridCount": true
+            },
+            "export": {
+                "enabled": true,
+                "position": "bottom-left"
+            },
+            "valueScrollbar":{
+
+            }
+        });
+
+        showPanelLoad('portletGraphic',false);
+    }
+}
+
+function getdata(dataNew) {
+    let chartData = [];
+    let arrayDate = dateFile.split("-");
+    let hourarray = hourFile.split(":");
+    dataNew.forEach((i) => {
+        let date = new Date(arrayDate[0],arrayDate[1],arrayDate[2]);
+        date.setHours(parseInt(hourarray[0]), 0, 1, 0);
+        let minutes = parseInt(i.x / 60);
+        let seconds = parseInt(i.x % 60);
+        let milisec = parseInt(((i.x % 60) - parseInt(i.x %60)) * 1000);
+        date.setMinutes(minutes);
+        if(i.x !== 0) {
+            date.setSeconds(seconds);
+            date.setMilliseconds(milisec);
+        }
+        let dataFinale;
+        switch (axis){
+            case "BH1":
+                dataFinale = {
+                    "date": date,
+                    "BH1": i.y,
+                };
+                break;
+            case "BH2":
+                dataFinale = {
+                    "date": date,
+                    "BH2": i.y,
+                };
+                break;
+            case "BHZ":
+                dataFinale = {
+                    "date": date,
+                    "BHZ": i.y,
+                };
+                break;
+        }
+
+        chartData.push(dataFinale);
+    });
+    return chartData;
+}
