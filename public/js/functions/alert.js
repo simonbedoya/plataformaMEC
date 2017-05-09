1f13c75131049548c4597380e87ef61493a9f865
simon bedoya <simonbedoya@unicauca.edu.co>
2017-05-09 05:52:11 -0500

    09/05/17

--- public/js/functions/alert.js
+++ public/js/functions/alert.js
@@ -1,11 +1,13 @@
 /**
  * Created by sbv23 on 01/05/2017.
  */
+let sensors;
 
-function load(email,networks,sensors) {
+function load(email,networks,sensorsList) {
+    sensors = sensorsList;
     reloadNumberNoReadNotification(email);
     loadNetwork(networks);
-    loadSensor(sensors);
+    loadSensor(sensorsList);
 }
 
 function loadNetwork(data) {
@@ -15,9 +17,25 @@ function loadNetwork(data) {
     }
 }
 
-function loadSensor(data) {
+function loadSensor(data){
     let i;
     for(i = 0; i < data.length; i++) {
         document.getElementById("filterSensor").innerHTML += "<option value='" + data[i].PK_SENSOR + "'>"+data[i].NAME_SENSOR+"</option>";
+    }
+}
+
+function selectNetwork() {
+    let selectNet = $('#filterNetwork').val();
+    if(selectNet === 0){
+        loadSensor(sensors);
+    }else{
+        let sensorAux = [];
+        let i;
+        for(i = 0; i < sensors.length; i++) {
+            if(sensors[i].PK_NETWORK = selectNet){
+                sensorAux.push(sensors[i]);
+            }
+        }
+        loadSensor(sensorAux);
     }
 }
